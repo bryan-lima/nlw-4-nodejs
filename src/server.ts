1fea53f076b219caf91624fcc8edcf1c9574912a
Bryan <bryanlima200@outlook.com>
2021-03-02 14:40:35 -0300

    Initial route settings (POST method)

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express, { response } from "express";
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is running!"));
-
 app.get("/", (request, response) => {
     return response.json({"message": "Hello World!"});
-})
+});
+
+app.post("/", (request, response) => {
+    return response.json({"message": "Dados salvos com sucesso!"});
+});
+
+app.listen(3333, () => console.log("Server is running!"));
