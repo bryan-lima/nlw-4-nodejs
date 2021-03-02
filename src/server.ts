b586d69c7e2929bdd17ac5025e5367404d165dd1
Bryan <bryanlima200@outlook.com>
2021-03-02 18:44:23 -0300

    Moved route settings from server.ts to routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import "reflect-metadata";
 import express, { response } from "express";
 import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({"message": "Hello World!"});
-});
-
-app.post("/", (request, response) => {
-    return response.json({"message": "Dados salvos com sucesso!"});
-});
+app.use(router)
 
 app.listen(3333, () => console.log("Server is running!"));
