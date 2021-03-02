3a6989ee867352364a805d7a99ba84a969647004
Bryan <bryanlima200@outlook.com>
2021-03-02 14:16:44 -0300

    Initial route settings (GET method)

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 
 app.listen(3333, () => console.log("Server is running!"));
+
+app.get("/", (request, response) => {
+    return response.json({"message": "Hello World!"});
+})
