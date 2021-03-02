37cd0dd63782cfd12fa559461c3cb3e707653728
Bryan <bryanlima200@outlook.com>
2021-03-02 17:37:06 -0300

    Created migration CreateUsers

---
+++ src/database/migrations/1614717135406-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1614717135406 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
