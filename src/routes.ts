af4319c7ce631208e64abca1739e6674c1120bf3
Bryan <bryanlima200@outlook.com>
2021-03-03 18:26:41 -0300

    Added route to list all surveys (GET method)

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,8 @@ const userController = new UserController();
 const surveysController = new SurveysController();
 
 router.post("/users", userController.create);
+
 router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export { router };
