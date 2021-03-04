9096e98313b3631c8f1839d5143de88362606b27
Bryan <bryanlima200@outlook.com>
2021-03-04 16:14:57 -0300

    Created migration CreateSurveysUsers

---
+++ src/database/migrations/1614885042201-CreateSurveysUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateSurveysUsers1614885042201 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
