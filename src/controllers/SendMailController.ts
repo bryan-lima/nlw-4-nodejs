148a76b61a94affdda9b96e532957eb8d76e56f9
Bryan <bryanlima200@outlook.com>
2021-03-06 01:19:41 -0300

    Separate responsibilities for SendMailController and SendMailService For the SendMail controller, the responsibility to inform the handlebar path and the value of the variables that will be used by the SendMail service, which must be more generic, making it possible to be reused for sending emails with another purpose, if necessary

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { resolve } from "path";
 import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
@@ -13,9 +14,9 @@ class SendMailController {
         const surveysRepository = getCustomRepository(SurveysRepository);
         const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-        const userAlreadyExists = await usersRepository.findOne({ email });
+        const user = await usersRepository.findOne({ email });
 
-        if(!userAlreadyExists) {
+        if(!user) {
             return response.status(400).json({
                 error: "User does not exists!",
             });
@@ -30,13 +31,21 @@ class SendMailController {
         }
 
         const surveyUser = surveysUsersRepository.create({
-            user_id: userAlreadyExists.id,
+            user_id: user.id,
             survey_id,
         });
 
         await surveysUsersRepository.save(surveyUser);
+        
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
 
-        await SendMailService.execute(email, survey.title, survey.description);
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description
+        };
+
+        await SendMailService.execute(email, survey.title, variables, npsPath);
 
         return response.json(surveyUser);
     }
